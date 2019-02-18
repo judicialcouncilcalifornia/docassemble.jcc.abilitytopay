52fe4575bf765ef77a9ec469e07dbbfa282b256c
Jared White <jared@jaredwhite.com>
2019-02-18 12:09:51 -0800

    Styled help tip and widget examples

--- docassemble/jcc/abilitytopay/data/static/a2p.js
+++ docassemble/jcc/abilitytopay/data/static/a2p.js
@@ -1,4 +1,9 @@
 /* DocAssemble page load event */
 $(document).on('daPageLoad', function(){
   $('[data-toggle="tooltip"]').tooltip();
+  $('a.help-widget').click(function(e) {
+    e.preventDefault();
+    $(this).addClass('opened');
+    $(this).next().show();
+  });
 });
