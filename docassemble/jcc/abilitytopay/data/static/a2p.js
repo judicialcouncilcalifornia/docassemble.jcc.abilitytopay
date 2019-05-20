f57f252dff2d4baa266e96c144dfbb1ed25f3669
Benny Lichtner <bennlich@gmail.com>
2019-05-19 21:47:58 -0700

    fix idle timeout bug; cc @JCCChris a new interval was getting created on every page load, causing the counter to increment several times every minute

--- docassemble/jcc/abilitytopay/data/static/a2p.js
+++ docassemble/jcc/abilitytopay/data/static/a2p.js
@@ -12,17 +12,16 @@ $(document).on('daPageLoad', function(){
  * after an idle timeout of 60 minutes. */
 
 var idleTime = 0;
-$(document).on('daPageLoad', function () {
-    //Increment the idle time counter every minute.
-    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
 
-    //Zero the idle timer on mouse movement.
-    $(this).mousemove(function (e) {
-        idleTime = 0;
-    });
-    $(this).keypress(function (e) {
-        idleTime = 0;
-    });
+//Increment the idle time counter every minute.
+var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+//Zero the idle timer on mouse movement.
+$(window).mousemove(function (e) {
+    idleTime = 0;
+});
+$(window).keypress(function (e) {
+    idleTime = 0;
 });
 
 function timerIncrement() {
