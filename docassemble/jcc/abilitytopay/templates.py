7e0dd2198b0c4c4b5467ab22d7de7e9c3b16a707
Benny Lichtner <bennlich@gmail.com>
2019-06-25 23:44:41 -0700

    add templates.py for functions that generate html snippets; add a2p_file_input_html for generating a custom file input

---
+++ docassemble/jcc/abilitytopay/templates.py
@@ -0,0 +1,18 @@
+from docassemble.base.util import encode_name
+import json
+
+def a2p_file_input_html(file_variable_name):
+  return """
+<div class="a2p-file-upload-container">
+  <div class="a2p-upload-complete"><span class="a2p-upload-complete-icon fas fa-check-circle"></span>Upload Complete</div>
+  <img class="a2p-image-preview" />
+  <label class="a2p-file-upload-button btn btn-primary btn-block" for="{encoded_variable_name}">
+    <span class="a2p-file-upload-label">Add a photo</span> <span style="position: relative; top: 2px; margin-left: 6px; font-size: 20px" class="fas fa-camera"></span>
+    <input class="a2p-file-input" type="file" name="{encoded_variable_name}" id="{encoded_variable_name}">
+    <input type="hidden" name="_files" value="{encoded_list}"/>
+  </label>
+</div>
+""".format(
+  encoded_variable_name=encode_name(file_variable_name),
+  encoded_list=encode_name(json.dumps([encode_name(file_variable_name)]))
+)
