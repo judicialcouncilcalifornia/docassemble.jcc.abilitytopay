cf8b4c34ea5ab3e60612074bd90bb5a736684733
Benny Lichtner <bennlich@gmail.com>
2019-06-26 17:12:16 -0700

    remove old help_widget code

--- docassemble/jcc/abilitytopay/help_widget.py
+++
@@ -1,5 +0,0 @@
-def help_tip(help_text):
-    return '<span class="help-tip">{0}</span>'.format(help_text)
-
-def help_widget(disclosure_text, help_heading, help_text):
-    return '<a href="#" class="help-widget">{0}</a><div class="help-widget-body"><h5>{1}</h5>{2}</div>'.format(disclosure_text, help_heading, help_text)
