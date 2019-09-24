0832605cb0f81987f4ccf9dc40a517aec5f8740a
Benny Lichtner <bennlich@gmail.com>
2019-09-24 13:07:58 -0700

    oops; update write_version.py

--- write_version.py
+++ write_version.py
@@ -6,11 +6,15 @@ VERSION = sys.argv[1]
 
 version_file_path = "./docassemble/jcc/abilitytopay/data/static/version.js"
 version_string = """
+//
+// This file is re-generated automatically by write_version.py
+//
+
 var A2P_PUBLIC_UI_VERSION='{0}';
-console.log('Ability to Pay ' + A2P_PUBLIC_UI_VERSION);
+console.log('ATP Public UI ' + A2P_PUBLIC_UI_VERSION);
 """.format(VERSION)
 
 with open(version_file_path, "w+") as version_file:
-  version_file.write(version_string)
+    version_file.write(version_string)
 
 print("Wrote version {0} to {1}".format(VERSION, version_file_path))
