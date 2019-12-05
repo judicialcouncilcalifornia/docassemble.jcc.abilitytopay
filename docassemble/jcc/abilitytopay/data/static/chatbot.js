814f1333d80ab078d25239e92d21d4d896c0294d
Benny Lichtner <bennlich@gmail.com>
2019-12-05 13:49:45 -0800

    update chatbot.js

--- docassemble/jcc/abilitytopay/data/static/chatbot.js
+++ docassemble/jcc/abilitytopay/data/static/chatbot.js
@@ -1,5 +1,5 @@
 // CENSUS 2020 CHATBOT CODE
-$(document).ready(function() {    
+$(document).ready(function() {
     var frame = null;
 
     (function () {
@@ -10,26 +10,18 @@ $(document).ready(function() {
 
         var div = document.createElement("div");
         document.getElementsByTagName('body')[0].appendChild(div);
-        div.outerHTML = "<div id='botDiv' style='z-index: 2000; border-radius: 20px; height: 60px; position: fixed; bottom: 0; right: 10px;  background: rgb(36, 79, 114, 0.95);'>" +
-            "<div id='botTitleBar' style='height: 60px; width: " + frameWidth + "px; position:fixed; cursor: pointer;'></div>" +
-            "<iframe id='iframeBot' width='" + frameWidth + "px' height='" + frameHeight + "px' src='https://jccatpchatbotwebprod.azurewebsites.net/chat.html'></iframe></div>";
-
-        document.querySelector('body').addEventListener('click', function (e) {
-            e.target.matches = e.target.matches || e.target.msMatchesSelector;
-            if (e.target.matches('#botTitleBar')) {
-                var botDiv = document.querySelector('#botDiv');
-                frame = document.getElementById('iframeBot').contentWindow;
-                if (botDiv.style.height == frameHeight + 'px') {
-                    botDiv.style.height = '60px';
-                    var postMessageData = { 'event': 'resize', 'action': 'minimize' };
-                    frame.postMessage(JSON.stringify(postMessageData), window.location.href);
-                } else {
-                    botDiv.style.height = frameHeight + 'px';
-                    var postMessageData = { 'event': 'resize', 'action': 'maximize' };
-                    frame.postMessage(JSON.stringify(postMessageData), window.location.href);
-                }
-                botDiv.style.overflow = "hidden";
-            };
-        });
+        div.outerHTML = "<iframe class='iframeBot' id='iframeBot' width='"+frameWidth+"px' height='"+frameHeight+"px' style='position:fixed; bottom: 0; right: 10px; border-radius: 20px; border: 0; background: unset;' src='https://jccatpchatbotwebprod.azurewebsites.net/chat.html'></iframe>";
     }());
+
+    function changeLanguageSelected(e) {
+        var languageSelected = e.options[e.selectedIndex].value;
+        var postMessageData = { type: 'state', language: languageSelected, county: '', questionId: '' };
+        if (frame == null) {
+            frame = document.getElementById('iframeBot').contentWindow;
+        }
+
+        if (frame != null) {
+            frame.postMessage(JSON.stringify(postMessageData), window.location.href);
+        }
+    }
 });
