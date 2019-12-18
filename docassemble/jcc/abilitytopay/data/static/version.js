6e035eea6c598a8fd537109d3f0a71222f4eb98c
Benny Lichtner <bennlich@gmail.com>
2019-12-18 00:27:02 -0800

    1.2.9

--- docassemble/jcc/abilitytopay/data/static/version.js
+++ docassemble/jcc/abilitytopay/data/static/version.js
@@ -3,5 +3,5 @@
 // This file is re-generated automatically by write_version.py
 //
 
-var A2P_PUBLIC_UI_VERSION='1.2.8';
+var A2P_PUBLIC_UI_VERSION='1.2.9';
 console.log('ATP Public UI ' + A2P_PUBLIC_UI_VERSION);
