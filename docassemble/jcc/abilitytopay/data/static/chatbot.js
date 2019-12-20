3751b04107924dba4d92825cae4676b7716792ec
JessicaLozada <51461225+JessicaLozada@users.noreply.github.com>
2019-12-20 16:30:34 -0600

    Update chatbot.js
    Fixing frame size variables scope: Move variables (deviceWidth, deviceHeight, frameWidth and frameHeight) out of the function.
    Fixing of Continue button blocked: Change width and height of the iframe with the minimal for the first load of the screen.

--- docassemble/jcc/abilitytopay/data/static/chatbot.js
+++ docassemble/jcc/abilitytopay/data/static/chatbot.js
@@ -1,16 +1,15 @@
-// CENSUS 2020 CHATBOT CODE
+// JCC ATP CHATBOT CODE
 $(document).ready(function() {
     var frame = null;
-
+    var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    var deviceHeight = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+    var frameWidth = (deviceWidth < 420) ? (deviceWidth - 20) : 440;
+    var frameHeight = (deviceHeight < 830) ? (deviceHeight - 50) : 750;
+    
     (function () {
-        var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        var deviceHeight = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-        var frameWidth = (deviceWidth < 420) ? (deviceWidth - 20) : 440;
-        var frameHeight = (deviceHeight < 830) ? (deviceHeight - 50) : 750;
-
         var div = document.createElement("div");
         document.getElementsByTagName('body')[0].appendChild(div);
-        div.outerHTML = "<iframe class='iframeBot' id='iframeBot' width='"+frameWidth+"px' height='"+frameHeight+"px' style='position:fixed; bottom: 0; right: 10px; border-radius: 20px; border: 0; background: unset; z-index: 2000;' src='https://jccatpchatbotwebprod.azurewebsites.net/chat.html'></iframe>";
+        div.outerHTML = "<iframe class='iframeBot' id='iframeBot' width='140px' height='60px' style='position:fixed; bottom: 0; right: 10px; border-radius: 20px; border: 0; background: unset; z-index: 2000;' src='https://jccatpchatbotwebprod.azurewebsites.net/chat.html'></iframe>";
     }());
 
     function changeLanguageSelected(e) {
